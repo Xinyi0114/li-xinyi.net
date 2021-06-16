887152db7cda69b2ddd680f6f3723b97c81e0e03
Xinyi0114 <lixinyi.1203@gmail.com>
2021-06-16 10:42:10 +0200

    test

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
           //$(this).tooltip({content:'	<img src="thumbnail/rape.png"  >'})
           //let thumbnail = $(this).find("img")
           $(this).tooltip({
-              items: '.item',
-              content: function()
-              {
-                let $thumbnail = $(this).find("img").get(0)
-                $(thumbnail).css("width","500px")
-                return $thumbnail;
-              },
+            items: '.item',
+            content: function()
+            {
+              var thumbnail = $(this).find("img").get(0);
+      $(thumbnail).css('width', '500px');
+              return thumbnail;
+            },
               show: null, // show immediately
               track:true,//follow the mouse
               open: function(event, ui)
