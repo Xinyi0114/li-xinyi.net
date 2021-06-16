e19a15f88fa2a85dc7936b31061bbf682581fa27
Xinyi0114 <lixinyi.1203@gmail.com>
2021-06-16 16:08:04 +0200

    open

--- script.js
+++ script.js
@@ -12,33 +12,7 @@ $(document).ready(function(){
               },
               show: null, // show immediately
               track:true,//follow the mouse
-              open: function(event, ui)
-              {
-                  if (typeof(event.originalEvent) === 'undefined')
-                  {
-                      return false;
-                  }
 
-                  var $id = $(ui.tooltip).attr('id');
-                  // close any lingering tooltips
-                  $('div.ui-tooltip').not('#' + $id).remove();
-
-                  // ajax function to pull in data and add it to the tooltip goes here
-              },
-              close: function(event, ui)
-              {
-                  ui.tooltip.hover(function()
-                  {
-                      $(this).stop(true).fadeTo(200, 1);
-                  },
-                  function()
-                  {
-                      $(this).fadeOut('200', function()
-                      {
-                          $(this).remove();
-                      });
-                  });
-              }
           });
 
       });
