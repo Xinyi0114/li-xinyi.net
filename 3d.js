5c015db06502d39dad085b5abfdbf0dbe3a176d6
Xinyi0114 <lixinyi.1203@gmail.com>
2021-06-24 19:58:18 +0200

    three

--- 3d.js
+++ 3d.js
@@ -1,7 +1,19 @@
 import * as THREE from './three.js-master/build/three.module.js';
-//import {GLTFLoader} from './three.js-master/examples/jsm/loaders/GLTFLoader.js';
+import {GLTFLoader} from './three.js-master/examples/jsm/loaders/GLTFLoader.js';
+import {FBXLoader} from './three.js-master/examples/jsm/loaders/FBXLoader.js';
 import {OrbitControls} from './three.js-master/examples/jsm/controls/OrbitControls.js';
-//import { CSS2DRenderer, CSS2DObject } from './three.js-master/examples/jsm/renderers/CSS2DRenderer.js';
+import { CSS2DRenderer, CSS2DObject } from './three.js-master/examples/jsm/renderers/CSS2DRenderer.js';
+
+const GLTFloader = new GLTFLoader();
+const FBXloader = new FBXLoader();
+FBXloader.load( '3d/Female.fbx', function (fbx) {
+  scene.add( fbx.scene );
+  fbx.scene.position.set(10, 6, -10);
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+} );
 
 scene = new THREE.Scene()
   const ambientLight = new THREE.AmbientLight(0x777777)
