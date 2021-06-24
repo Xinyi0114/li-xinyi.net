e7c8e888d6e378aa98e3fc6d42e87cefb1ae21ef
Xinyi0114 <lixinyi.1203@gmail.com>
2021-06-24 22:33:47 +0200

    3d

--- 3d.js
+++ 3d.js
@@ -2,13 +2,13 @@ import * as THREE from './three.js-master/build/three.module.js';
 import {GLTFLoader} from './three.js-master/examples/jsm/loaders/GLTFLoader.js';
 import {FBXLoader} from './three.js-master/examples/jsm/loaders/FBXLoader.js';
 import {OrbitControls} from './three.js-master/examples/jsm/controls/OrbitControls.js';
-import { CSS2DRenderer, CSS2DObject } from './three.js-master/examples/jsm/renderers/CSS2DRenderer.js';
+import { CSS2DRenderer, CSS2DObject} from './three.js-master/examples/jsm/renderers/CSS2DRenderer.js';
 
 const GLTFloader = new GLTFLoader();
 const FBXloader = new FBXLoader();
-FBXloader.load( '3d/Female.fbx', function (fbx) {
+FBXloader.load( '3d/character.fbx', function (fbx) {
   scene.add( fbx.scene );
-  fbx.scene.position.set(10, 6, -10);
+  //fbx.scene.position.set(10, 6, -10);
 
 }, undefined, function ( error ) {
 
