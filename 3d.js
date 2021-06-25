1e3210f613b863a2a7137790e20fa0ce323770e2
Xinyi0114 <lixinyi.1203@gmail.com>
2021-06-25 13:22:28 +0200

    gltf

--- 3d.js
+++ 3d.js
@@ -70,7 +70,7 @@ function animate() {
 	renderer.render( scene, camera );
 }
 const gltfLoader = new GLTFLoader();
-const loadFemale = gltfLoader.load( './3d/female.gltf', function ( gltf ) {
+const loadFemale = gltfLoader.load( './3d/female2.gltf', function ( gltf ) {
   scene.add( gltf.scene );
   gltf.scene.position.set(10, 6, -10);
 }, undefined, function ( error ) {
